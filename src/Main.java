a89c3356aca35b119a05621d2572da352014ee47
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-12 10:51:20 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 1 ile 100 arasındaki sayıları döngü ile kontrol et
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asalMi = true;
+
+            // 2'den başlayarak sayının kendisine kadar olan sayılara böl
+            for (int bolen = 2; bolen < sayi; bolen++) {
+                // Eğer sayı, bolen'e tam bölünüyorsa, asal değildir
+                if (sayi % bolen == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            // Eğer sayı asalsa, yazdır
+            if (asalMi) {
+                System.out.println(sayi + " asal sayıdır.");
+            }
+        }
+    }
+
 }
